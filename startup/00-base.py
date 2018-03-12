47daa8ceef821bb3d9e962388c1c8add8843daf4
yugangzhang <yuzhang@bnl.gov>
2018-03-12 11:06:47 -0400

    use historydict again

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,6 +1,5 @@
 import nslsii
 from bluesky import RunEngine
-RE= RunEngine()
-RE.md = {}
+from bluesky.utils import get_history
+RE = RunEngine(get_history())
 nslsii.configure_base(get_ipython().user_ns, 'chx')
-
